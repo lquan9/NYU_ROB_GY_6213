f21d0dc6aaea167d847f861dfc70dfcae76e5f4c
Long Quang <longpquang@gmail.com>
2026-02-09 19:56:24 -0500

    merge lab2 to core

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -16,20 +16,14 @@ LABS = [
 @pytest.mark.parametrize("module_path", LABS)
 @pytest.fixture
 def user_interface(ui_run, module_path):
-    # 2. Dynamically import the lab module
     lab_module = importlib.import_module(module_path)
-    # Call the main_page function from that specific lab
     lab_module.main_page()
 
 @pytest.mark.parametrize("module_path", LABS)
 async def test_gui_elements_render(user_interface: UserInterface, module_path):
     """Check if the critical UI components exist on the page."""
     await user_interface.open('/')
-    
-    # Verify the Title
     await user_interface.should_see('ROB-GY - 6213: Robot Navigation & Localization')
-    
-    # Verify Switches and Labels exist
     await user_interface.should_see('Data Logging')
     await user_interface.should_see('Robot Connect')
     await user_interface.should_see('SPEED:')
